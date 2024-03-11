1917f1385827b0361fbca60ce12744bf7e4b6de5
Newgun <74343774+uiliugang@users.noreply.github.com>
2024-03-11 23:55:29 +0800

    updated: 改用正则匹配重定向URL

--- external-link-auto-redirect.js
+++ external-link-auto-redirect.js
@@ -16,16 +16,17 @@
 (function() {
     'use strict';
 
-    const redirectRegex = /[?&#](target|to|ac=2&url|url|remoteUrl|redirect|u|goto|link)=([^&]+)/i;
+    const redirectRegex = /^https?:\/\/.*\?.*https?/;
     const videoExtensions = ['.mp4', '.m3u8', '.avi', '.mov', '.wmv', '.mkv', '.flv', '.webm', '.mpeg', '.mpg', '.mp2v', '.m4v', '.svi', '.asx', '.wmv', '.wmx', '.m4p', '.m4b', '.mxf', '.roq', '.nsv', '.flv', '.mpe?g', '.mp3v', '.m1v', '.m2v', '.vob', '.ifo', '.dat', '.divx', '.cpk', '.dirac', '.drc', '.mj2', '.mjv', '.mod', '.tod', '.rec', '.uvh', '.uvu', '.qt', '.rm', '.ram', '.rpm', '.smil', '.ice', '.gifv', '.nsv', '.3ivx', '.3gpp', '.f4v', '.f4p', '.f4a', '.f4b', '.mxf', '.roq', '.nsv', '.flv', '.m4v', '.avi', '.dat', '.divx', '.cpk', '.dirac', '.drc', '.mj2', '.mjp2', '.mjv', '.mod', '.tod', '.rec', 'uvh', 'uvu', 'qt', 'mov', 'movie', 'rm', 'ra', 'ram', 'rpm', 'smil', 'ice', 'mpe?g', 'mp2v', 'mp3v', 'm4v', 'svi', '.asx', 'wmv', 'wmx', 'wm', 'asf', 'amv', 'mpv', 'm1v', 'm2v', 'vob', '.ifo'];
 
     function processUrl(redirectURL) {
         const matches = redirectURL.match(redirectRegex);
-        let realUrl = null;
-        if (matches && matches[2]) {
-            realUrl = decodeURIComponent(matches[2]);
+        console.log(`Matches: ${matches}`);
+        if (matches) {
+            let index = redirectURL.substring(4).indexOf("http")+3;
+            let realUrl = decodeURIComponent(redirectURL.substring(index + 1));
+            console.log(`Decoded URL: ${realUrl}`);
             if (isValidUrlAndNotVideo(realUrl)) {
-                console.log(`Matches: ${realUrl}`);
                 return realUrl;
             }
         }
@@ -78,6 +79,8 @@
                 window.open(processedUrl, '_blank');
             }
         }
+        console.log(`Original URL: ${url}`);
+        console.log(`Processed URL: ${processedUrl}`);
     }
 
     document.addEventListener('click', handleClick);
