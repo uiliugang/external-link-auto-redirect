8e11432575555cf44eb30ff2b4f5b62db7c966d7
Newgun <74343774+uiliugang@users.noreply.github.com>
2024-03-11 21:56:57 +0800

    修复:排除视频文件,避免影响部分网站浏览器播放

--- external-link-auto-redirect.js
+++ external-link-auto-redirect.js
@@ -2,7 +2,7 @@
 // @name         External Link Auto Redirect
 // @name:zh-CN   外链自动重定向
 // @namespace    http://tampermonkey.net/
-// @version      0.5
+// @version      0.6
 // @description  redirect to the real URL directly when clicking on a link that contains a redirect URL
 // @description:zh-CN  点击包含重定向 URL 的链接时，直接重定向到真实的 URL
 // @author       uiliugang
@@ -50,9 +50,6 @@
                 window.open(processedUrl, '_blank');
             }
         }
-        // 调试信息
-        console.log(`Original URL: ${url}`);
-        console.log(`Processed URL: ${processedUrl}`);
     });
 
     // 情形二：原网页显示的链接是直链，但是监听 click 事件，当点击时，会发生重定向
@@ -63,17 +60,42 @@
 
     function processUrl(redirectURL) {
         const matches = redirectURL.match(redirectRegex);
-        console.log(`Matches: ${matches}`);
         if (matches && matches[2]) {
-            // 判断 URL 是否合法
-            const urlRegex = /^(https?|ftp):\/\/(\S+)?/i;
             const realUrl = decodeURIComponent(matches[2]);
-            if (realUrl.match(urlRegex)) {
+            if (isValidUrlAndNotVideo(realUrl)) {
+                console.log(`Matches: ${matches}`, `Real URL: ${realUrl}`);
                 return realUrl;
-            }
+              } 
         }
         return null;
     }
+
+    function isValidUrlAndNotVideo(string) {
+        try {
+          // 创建URL对象
+          const url = new URL(string);
+          
+          // 获取URL的路径名
+          const pathname = url.pathname;
+          
+          // 定义一个包含常见视频文件扩展名的数组
+          const videoExtensions = ['.mp4', '.m3u8', '.avi', '.mov', '.wmv', '.mkv', '.flv', '.webm', '.mpeg', '.mpg', '.mp2v', '.m4v', '.svi', '.asx', '.wmv', '.wmx', '.m4p', '.m4b', '.mxf', '.roq', '.nsv', '.flv', '.mpe?g', '.mp3v', '.m1v', '.m2v', '.vob', '.ifo', '.dat', '.divx', '.cpk', '.dirac', '.drc', '.mj2', '.mjv', '.mod', '.tod', '.rec', '.uvh', '.uvu', '.qt', '.rm', '.ram', '.rpm', '.smil', '.ice', '.gifv', '.nsv', '.3ivx', '.3gpp', '.f4v', '.f4p', '.f4a', '.f4b', '.mxf', '.roq', '.nsv', '.flv', '.m4v', '.avi', '.dat', '.divx', '.cpk', '.dirac', '.drc', '.mj2', '.mjp2', '.mjv', '.mod', '.tod', '.rec', 'uvh', 'uvu', 'qt', 'mov', 'movie', 'rm', 'ra', 'ram', 'rpm', 'smil', 'ice', 'mpe?g', 'mp2v', 'mp3v', 'm4v', 'svi', '.asx', 'wmv', 'wmx', 'wm', 'asf', 'amv', 'mpv', 'm1v', 'm2v', 'vob', '.ifo'];
+      
+          // 检查路径名是否以视频文件扩展名结尾
+          for (const ext of videoExtensions) {
+            if (pathname.endsWith(ext)) {
+              return false; // 如果是视频文件扩展名，返回false
+            }
+          }
+      
+          // 如果不是视频文件，返回true
+          return true;
+        } catch (e) {
+          // 如果抛出异常，说明URL不合法，返回false
+          return false;
+        }
+      }
+
 })();
 
 
