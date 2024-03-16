8350569925090c9b56e68773328773c07a19fa2c
Newgun <74343774+uiliugang@users.noreply.github.com>
2024-03-16 15:14:15 +0800

    fixed some bugs

--- external-link-auto-redirect.js
+++ external-link-auto-redirect.js
@@ -2,7 +2,7 @@
 // @name         External Link Auto Redirect(Direct Link)
 // @name:zh-CN   外链自动重定向（默认直链）
 // @namespace    http://tampermonkey.net/
-// @version      1.3.1
+// @version      1.3.2
 // @description  redirect to the real URL directly when clicking on a link that contains a redirect URL
 // @description:zh-CN  点击包含重定向 URL 的链接时，直接跳转到到真实的 URL
 // @author       uiliugang
@@ -16,32 +16,46 @@
 (function() {
     'use strict';
 
-    const redirectRegex = /^https?:\/\/.*\?.*https?/;
-    const excludedKeyWords = ['.m3u8', '.flv', '.ts', 'login','sign','auth','logout','register','logout','download','upload','share','video','play','watch','stream','live','embed','api','callback', 'token'];
+    const httpPattern = /http/g;
+    const excludedKeyWords = ['.m3u8', '.flv', '.ts', 'portal', 'qrcode', 'login','sign','auth','logout','register','upload','share','video','player','play','watch','stream','live','api','callback', 'token'];
 
     function processUrl(redirectURL) {
-        const matches = redirectURL.match(redirectRegex);
-        console.log(`Matches: ${matches}`);
-        if (matches) {
-            let index = redirectURL.substring(4).indexOf("http")+3;
-            let realUrl = decodeURIComponent(redirectURL.substring(index + 1));
-            //console.log(`Decoded URL: ${realUrl}`);
-            if (isValidUrlAndNotExclude(realUrl)) {
+        let index = findSecondHttpPosition(redirectURL);
+        if (index !== -1) {
+            for (const ext of excludedKeyWords) {
+                if (redirectURL.includes(ext)) {
+                    // console.log(`Excluded Keyword: ${ext}`);
+                    return null;
+                }
+            }
+            let realUrl = decodeURIComponent(redirectURL.substring(index));
+            // console.log(`Decoded URL: ${realUrl}`);
+            if (isValidUrl(realUrl)) {
                 return realUrl;
             }
         }
         return null;
     }
 
-    function isValidUrlAndNotExclude(string) {
-        try {
-            const url = new URL(string);
-            const pathname = url.pathname;
-            for (const ext of excludedKeyWords) {
-                if (pathname.includes(ext)) {
-                    return false;
-                }
+    function findSecondHttpPosition(redirectURL) {
+        let match;
+        let position = -1;
+        let count = 0;
+        // console.log(`redirectURL : ${redirectURL}`);
+
+        while ((match = httpPattern.exec(redirectURL)) !== null) {
+            count++;
+            if (count === 2) {
+                position = match.index;
+                return position;
             }
+        }
+        return -1;
+    }
+
+    function isValidUrl(realUrl) {
+        try {
+            const url = new URL(realUrl);
             return true;
         } catch (e) {
             return false;
@@ -57,8 +71,8 @@
                 window.open(processedUrl, '_blank');
             }
         }
-        //console.log(`Original URL: ${element.href}`);
-        //console.log(`Processed URL: ${processedUrl}`);
+        // console.log(`Original URL: ${element.href}`);
+        // console.log(`Processed URL: ${processedUrl}`);
     });
 
     let processedUrl = processUrl(window.location.href);
