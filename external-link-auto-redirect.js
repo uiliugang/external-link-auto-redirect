3d545f3d84b6285046ca2c24d3190fe7486db7cf
Newgun <74343774+uiliugang@users.noreply.github.com>
2024-04-02 23:13:17 +0800

    bugs fixed

--- external-link-auto-redirect.js
+++ external-link-auto-redirect.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         External Link Auto Redirect(Direct Link)
-// @name:zh-CN   外链自动重定向（默认直链）
+// @name         External Link Auto Redirect
+// @name:zh-CN   外链自动重定向
 // @namespace    http://tampermonkey.net/
-// @version      1.3.3
+// @version      1.3.4
 // @description  redirect to the real URL directly when clicking on a link that contains a redirect URL
 // @description:zh-CN  点击包含重定向 URL 的链接时，直接跳转到到真实的 URL
 // @author       uiliugang
@@ -13,24 +13,37 @@
 // @updateURL https://update.greasyfork.org/scripts/462796/External%20Link%20Auto%20Redirect.meta.js
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
 
     const httpPattern = /http/g;
-    const excludedKeyWords = ['.m3u8', '.flv', '.ts', 'portal', 'qrcode', 'login','sign','auth','logout','register','upload','share','video','player','play','watch','stream','live','api','callback', 'token'];
+    const firstHttpExcludeWords = ['portal', 'token', 'sorry', 'qrcode', 'account', 'login', 'sign', 'auth', 'logout', 'register', 'upload', 'share', 'video', 'player', 'play', 'watch', 'stream', 'live', 'api', 'callback'];
+    const secondHttpExcludeWords = ['.m3u8', '.flv', '.ts']
 
-    function processUrl(redirectURL) {
+
+    function parseUrl(redirectURL) {
         let index = findSecondHttpPosition(redirectURL);
         if (index !== -1) {
-            let url = redirectURL.toLowerCase();
-            for (const ext of excludedKeyWords) {
-                if (url.includes(ext)) {
-                    console.log(`Excluded Keyword: ${ext}`);
+            let realUrl = redirectURL.substring(index);
+            let firstHttp = redirectURL.substring(0, index).toLowerCase();
+            let secondHttp = realUrl.toLowerCase();
+
+            for (const ext of firstHttpExcludeWords) {
+                if (firstHttp.includes(ext)) {
+                    console.log(`firstHttpExcludeWord: ${ext}`);
                     return null;
                 }
             }
-            let realUrl = decodeURIComponent(redirectURL.substring(index));
-            console.log(`Decoded URL: ${realUrl}`);
+
+            for (const ext of secondHttpExcludeWords) {
+                if (secondHttp.includes(ext)) {
+                    console.log(`secondHttpExcludeWord: ${ext}`);
+                    return null;
+                }
+            }
+
+            realUrl = decodeURIComponent(realUrl);
+
             if (isValidUrl(realUrl)) {
                 return realUrl;
             }
@@ -42,12 +55,12 @@
         let match;
         let position = -1;
         let count = 0;
-        console.log(`redirectURL : ${redirectURL}`);
 
         while ((match = httpPattern.exec(redirectURL)) !== null) {
             count++;
             if (count === 2) {
                 position = match.index;
+                console.log(`Redirect URL: ${redirectURL}`);
                 return position;
             }
         }
@@ -63,21 +76,21 @@
         }
     }
 
-    document.addEventListener('click', function(e) {
+    document.addEventListener('click', function (e) {
         const element = e.target.closest('a[href]');
         if (element) {
-            const processedUrl = processUrl(element.href);
-            if (processedUrl) {
+            const parsedUrl = parseUrl(element.href);
+            if (parsedUrl) {
                 e.preventDefault();
-                window.open(processedUrl, '_blank');
+                window.open(parsedUrl, '_blank');
             }
         }
-        console.log(`Original URL: ${element.href}`);
-        console.log(`Processed URL: ${processedUrl}`);
     });
 
-    let processedUrl = processUrl(window.location.href);
-    if (processedUrl) {
-        window.location.replace(processedUrl);
+    let parsedUrl = parseUrl(window.location.href);
+    if (parsedUrl) {
+        window.location.replace(parsedUrl);
     }
+
+    console.log(`parsed URL: ${parsedUrl}`);
 })();
